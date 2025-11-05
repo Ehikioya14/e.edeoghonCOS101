9e566b4cf822ac2f1e8072020be3ce8ddea550db
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-05 19:29:32 +0100

    (String Literal)

--- week-4/Project_2/src/main.rs
+++ week-4/Project_2/src/main.rs
@@ -2,31 +2,29 @@ use std::io;
 
 fn main() {
     // Step 1: Get the user's input
-    println!("Calculator for Annual Incentives");
+    println!("\nCalculator for Annual Incentives");
     let mut experience_input = String::new();
     let mut age_input = String::new();
     
-    println!("Does the employee have experience (yes/no):");
+    println!("\nDoes the employee have experience (yes/no):");
     io::stdin().read_line(&mut experience_input).expect("Failed to read input");
     let experience = experience_input.trim().to_lowercase();
 
-
-    
     if experience == "yes" {
-    println!("Enter the age of the employee");
+    println!("\nEnter the age of the employee");
     io::stdin().read_line(&mut age_input).expect("Failed to read input");
     let age:u32 = age_input.trim().parse().expect("Please enter a valid age");
 
     // Step 2: use decision making to determine the incentives
    
         if age >= 40 {
-            println!("The incentive of the employee is N1,560,000");
+            println!("\nThe incentive of the employee is N1,560,000");
         } else if age >= 30 && age < 40 {
-            println!("The incentive of the employee is N1,480,000");
+            println!("\nThe incentive of the employee is N1,480,000");
         } else if age < 28 {
-            println!("The incentive of the employee is N1,300,000");
+            println!("\nThe incentive of the employee is N1,300,000");
         }
     } else if experience == "no"{
-        println!("The incentive of the employee is N100,000");
+        println!("\nThe incentive of the employee is N100,000");
     }
 }
