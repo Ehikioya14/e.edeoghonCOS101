2fc43d3cf5097acd5dcc376a3ec8d264b381eca1
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-12 16:58:05 +0100

    Function with parameters

---
+++ week-7/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-7\practice_4\target\debug\practice_4.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-7\practice_4\src\main.rs
