2bfffc205633cbab4b45ce61e1573485c5a73528
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-12 17:24:18 +0100

    Declaring and Initializing arrays

---
+++ week-7/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-7\practice_8\target\debug\practice_8.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-7\practice_8\src\main.rs
