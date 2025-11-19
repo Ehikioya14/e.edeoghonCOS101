5b4d8b353acee0c8d0b9a0cacc315b9e98dd7312
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-19 16:30:37 +0100

    Creating a vector in rust

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // Using Vec::new()
+    let v4 : Vec<i64> = Vec::new();
+
+    // printing the size of vector
+    println!("\nThe length of Vec::new is: {}",v4.len());
+
+    // Usinh macro
+    let v5 = vec!["Grace","Effiong","Basil","Kareem","Susan"];
+
+    // printing the size of vector
+    println!("\nThe lenght of vec macro is: { }",v5.len());
+
+}
---
+++ week-8/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-8\practice_1\target\debug\practice_1.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-8\practice_1\src\main.rs
