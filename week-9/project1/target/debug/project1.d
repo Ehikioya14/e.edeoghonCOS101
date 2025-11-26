541d91bca10570c4f890c3da92548737daab37b9
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-26 16:34:55 +0100

    Creating and Writing to a file

---
+++ week-9/project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-9\project1\target\debug\project1.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-9\project1\src\main.rs
