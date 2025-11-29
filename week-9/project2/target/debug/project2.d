de7a6bae5ebaf3d536013b7d4f2f7fb3187918aa
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-11-29 23:51:41 +0100

    Finished project 2 with vectors

---
+++ week-9/project2/target/debug/project2.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-9\project2\target\debug\project2.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-9\project2\src\main.rs
