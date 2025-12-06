d42adc79b2f1df4e8fc49e6707152299c3ce8cf7
unknown <Ehikioya.Edeoghon@pau.edu.ng>
2025-12-06 20:12:42 +0100

    Finished project using structs and method in structure

---
+++ week-10/project/src/main.rs
@@ -0,0 +1,28 @@
+// defining the brand of laptops
+struct Price {
+    hp:u32, ibm:u32, tosh:u32, dell:u32
+}
+
+//logic to calculate total cot of laptops if a customer purchases 3 of each brand
+impl Price{
+    fn sum(&self)->u32 {
+        (self.hp * 3) + (self.ibm * 3) + (self.tosh * 3) + (self.dell * 3)
+    }
+}
+
+
+fn main() {
+    //instantiate it
+    let laptops = Price {
+        hp:650_000,
+        ibm:750_000,
+        tosh:550_000,
+        dell:850_000
+    };
+    // print the sum
+    println!("\nHey!");
+    println!("The price of an hp laptop is {} \n 
+        The price of an ibm laptop is {} \n The price of a Toshiba laptop is {} \n 
+        The price of a dell laptop is {} \n The total cost if a customer buys 3 of each brand is {} ",laptops.hp,laptops.ibm,laptops.tosh,laptops.dell,laptops.sum());
+
+}
---
+++ week-10/project/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-10\project\target\debug\project.exe: C:\Users\JOHN\ EDEOGHON\OneDrive\Documents\e.edeoghonCOS101\week-10\project\src\main.rs
